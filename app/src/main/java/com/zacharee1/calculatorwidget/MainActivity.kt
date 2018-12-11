194b336b75edc4791ea20bd511d53baf911701a6
Zachary Wander <zachary.wander@gmail.com>
2018-12-11 15:56:31 -0500

    Update dependencies, implement Crashlytics

---
+++ app/src/main/java/com/zacharee1/calculatorwidget/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.zacharee1.calculatorwidget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
